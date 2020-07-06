2d5e8a833c607c8a9107c0ddd2fa94ea650e0242
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-06 12:38:53 +0530

    Add files via upload

---
+++ carousel/app.js
@@ -0,0 +1,43 @@
+const arrowl = document.querySelector(".iconl");
+const arrowr = document.querySelector(".iconr");
+
+const pic = document.querySelector(".img");
+
+var i=1
+
+arrowl.addEventListener("click",function(){
+    if(i==1){
+        i=7;
+    }
+    else{
+        i=i-1;
+    }
+
+    pic.src="/images/pic-" + i + ".jpg";
+});
+
+arrowr.addEventListener("click",function(){
+    if(i==7){
+        i=1;
+    }
+    else{
+        i=i+1;
+    }
+
+    pic.src="/images/pic-" + i + ".jpg";
+});
+
+const auto = function(){
+    setInterval(function(){
+        if(i==7){
+            i=1;
+        }
+        else{
+            i=i+1;
+        }
+    
+        pic.src="/images/pic-" + i + ".jpg";
+    },2000);
+}
+
+auto();
